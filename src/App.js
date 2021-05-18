c4a5cbc370403927d8f995c7ed2b1c881c0e88ec
kms-payple <kms@payple.kr>
2021-05-18 18:07:37 +0900

    add  file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "./About";
 import Home from "./Home";
+import Plus from "./Plus";
 import { Route, Link } from "react-router-dom";
 
 function App() {
@@ -13,9 +14,13 @@ function App() {
                 <li>
                     <Link to="/about">어바웃</Link>
                 </li>
+                <li>
+                    <Link to="/plus">플러스</Link>
+                </li>
             </ul>
             <Route path="/" component={Home} exact />
             <Route path="/about" component={About} />
+            <Route path="/plus" component={Plus} />
         </div>
     );
 }
